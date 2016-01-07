cbe426dd53718de88fe8c10f48ab094066e778ba
sc1f <tanjunyuan@hotmail.co.uk>
2016-01-07 15:20:59 -0600

    finished dynamic captions, debugged and working.

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var updateScroll = debounce(function(){
     localStorage.setItem('scrollPosition', scrollPos.position);
     });
 }, 200, true);
-window.addEventListener('scroll', updateScroll)
+window.addEventListener('scroll', updateScroll);
 //navbar function, which reads the scrollTop() to decide when to change class from regular to expanded
 var updateNav = debounce(function(){
   var w = window.innerWidth;
@@ -17,27 +17,26 @@ var updateNav = debounce(function(){
       $('.nav-bar').addClass('nav-bar-scroll');
       $('#main-head').css('padding-top', '1vw').css('font-size','1.5vw');
       $('.contact-button').css('top','1.5vw');
-    }
-  else if ($(window).scrollTop() === 0 && w > 540) {
+    } else if ($(window).scrollTop() === 0 && w > 540) {
       $('.nav-bar').removeClass('nav-bar-scroll');
       $('#main-head').css('padding-top', '5vw').css('font-size','3vw');
       $('.contact-button').css('top','7.5vw');
-    }
-  else if ($(window).scrollTop() > 40 && w < 540) {
+    } else if ($(window).scrollTop() > 40 && w < 540) {
       $('.nav-bar').removeClass('nav-bar-scroll');
       $('#main-head').css('padding-top', '5vw').css('font-size','3vw');
       $('.contact-button').css('top','90%');
-    }
-  else if ($(window).scrollTop() === 0 && w < 540) {
+    } else if ($(window).scrollTop() === 0 && w < 540) {
       $('.nav-bar').removeClass('nav-bar-scroll');
       $('#main-head').css('padding-top', '5vw').css('font-size','3vw');
       $('.contact-button').css('top','90%');
     }
   });
 }, 200, true);
-window.addEventListener("scroll", updateNav);
-/*input validation: uses the inherent classes created by angularJS in an input form to see if the input fits angular's
-  built-in valudation checks. If the input is good, it will remove the disabled attribute from the button.*/
+window.addEventListener('scroll', updateNav);
+/*input validation: checks whether the textarea is deemed by Angular to have a value, and if true
+  it removes the disabled attribute from the submission button. If false, the button remains disabled.
+  The event handler runs this module every time a key is pressed.
+*/
 var lenCheck = debounce(function(){
   if($('md-input-container').hasClass('md-input-has-value') && $('.input').hasClass('ng-valid')){
     $('.activity-btn').removeAttr('disabled');
@@ -110,7 +109,7 @@ app.filter('unsafe', function($sce) {
     return $sce.trustAsHtml(val);
   };
 });
-//determines the window width and refactors the tooltip direction
+//determines the window width and refactors the tooltip direction (currently broken, needs debugging)
 app.controller('TooltipController', function($scope){
   $scope.direction = function($event){
    var w = window.innerWidth;
@@ -119,7 +118,7 @@ app.controller('TooltipController', function($scope){
    } else {
     return "bottom";
    }
-  }
+  };
 });
 //content storage, double line break = \n\n, break quotes = \
 app.controller("ModuleController", function() {
@@ -173,21 +172,21 @@ app.controller('TrackingController', function($scope){
   true if it is above 0, thus allowing the card to show.
   .*/
   var position = localStorage.getItem('scrollPosition');
-  var trackAction = function(){$(window).scrollTop(position)};
+  var trackAction = function(){$(window).scrollTop(position);};
   $scope.position = function($event){
     if(position > 0){
-      return true;
+      return "true";
     }
     else if(position === 0){
-      return false;
+      return "false";
     }
-  }
+  };
 });
 app.controller('FABController', function(){
   this.direction = "down";
   this.isOpen = "false";
   this.mode = "md-fling";
-})
+});
 app.controller('Activity1Controller', function(){
   (function(){
      $('#main-head').text('What did Columbus Look Like?');
@@ -239,52 +238,55 @@ app.directive('carousel', function($timeout) {
     },
     link: function(scope, elem, attrs){
       scope.currentIndex = 0;
-      scope.caption = function(){
-        $(document).ready(function() {
-      
-      if($('.carousel > .slide > img').length){
-        alert('found');
-      } else {
-        alert('not found');
-      }
-        var selected = $('.carousel>.slide>img');
-        if(selected.src === "img/carousel/1.jpg"){
-            $('#caption').text('Christopher Colombus, head-and-shoulders portrait, facing slightly right. Photomechanical print : halftone, color. Created between 1890 and 1940. Library of Congress');
-        } else if (selected.src === "img/carousel/2.jpg"){
-            $('#caption').text('First landing of Columbus on the shores of the New World: At San Salvador, W.I., Oct. 12th 1492 lithograph. Published by Currier & Ives, c.1892. Library of Congress');
-        } else if (selected.src === "img/carousel/3.jpg"){
-            $('#caption').text('[Christopher Columbus, half-length portrait, facing slightly right] photographic print [between 1900 and 1950] of a painting by Sebastiano, del Piombo, 1485-1547. Library of Congress.')
-        } else if (selected.src === "img/carousel/4.jpg"){
-            $('#caption').text('The landing of Columbus Oct. 11th 1492. Lithograph, hand-colored. Published by N. Currier, c. 1846. Library of Congress.');
-        } else if (selected.src === "img/carousel/5.jpg"){
-            $('#caption').text('The landing of Columbus 1492. Print. c. 1893 Jan. 26. Library of Congress.');
-        } else if (selected.src === "img/carousel/6.jpg"){
-            $('#caption').text('Christopher Columbus. Engraving by Johann Thedor DeBry. 16th century, Library of Congress.');
-        } else if (selected.src === "img/carousel/7.jpg"){
-            $('#caption').text('The landing of Columbus at San Salvador, October 12, 1492. Lithograph. Published by Currier & Ives, c. 1876. Library of Congress');
-        } else if (selected.src === "img/carousel/vanderlyn.jpg"){
-            $('#caption').text('John Vanderlyn, Landing of Christopher Columbus, U.S. Capitol, 1846');
-        } else if (selected.src === "img/carousel/rivera.jpg"){
-            $('#caption').text('Diego Rivera, Disembarkation of the Spanish at Vera Cruz (Colonial Domination), National Palace, Mexico City, 1951');
-        } else{
-            $('#caption').text();
-        }
-        });
-      };
+      //DYNAMIC CAPTIONS
+      //the array stores the strings of captions in the order they appear on the carousel.
+      var caption = [
+          //activity 1 carousel
+          'Christopher Colombus, head-and-shoulders portrait, facing slightly right. Photomechanical print : halftone, color. Created between 1890 and 1940. Library of Congress', 
+          'First landing of Columbus on the shores of the New World: At San Salvador, W.I., Oct. 12th 1492 lithograph. Published by Currier & Ives, c.1892. Library of Congress.',
+          'Christopher Columbus, half-length portrait, facing slightly right. Photographic print (between 1900 and 1950) of a painting by Sebastiano, del Piombo, 1485-1547. Library of Congress.',
+          'The landing of Columbus Oct. 11th 1492. Lithograph, hand-colored. Published by N. Currier, c. 1846. Library of Congress.',
+          'The landing of Columbus 1492. Print. c. 1893 Jan. 26. Library of Congress.',
+          'Christopher Columbus. Engraving by Johann Thedor DeBry. 16th century, Library of Congress.',
+          'The landing of Columbus at San Salvador, October 12, 1492. Lithograph. Published by Currier & Ives, c. 1876. Library of Congress',
+          //activity 4 carousel
+          'John Vanderlyn, Landing of Christopher Columbus, U.S. Capitol, 1846.',
+          'Diego Rivera, Disembarkation of the Spanish at Vera Cruz (Colonial Domination), National Palace, Mexico City, 1951.'
+          ];
       scope.next = function(){
+        //calculate the index of the current slide
         scope.currentIndex < scope.images.length -1 ? scope.currentIndex++ : scope.currentIndex = 0;
+        //+1 to current index so that it displays a number starting from 1
         scope.cleanIndex = scope.currentIndex + 1;
-        $('.number-display').text(scope.cleanIndex);    
+        //adjust the number display to show the new clean index on click
+        $('.number-display').text(scope.cleanIndex);
+        //detect whether the page is pointed at activity 4
+        if(window.location.href.indexOf("activity4") > -1) {
+          //if activity 4, display the string in the array index + 7 (to compensate for the fact that there are only 2 slides)
+          $('#caption').text(caption[scope.currentIndex + 7])
+        } else {
+          //if activity 1, display the string in the array's current index
+          $('#caption').text(caption[scope.currentIndex]);
+        }
       };
       scope.prev = function(){
+        //as above
         scope.currentIndex > 0? scope.currentIndex -- : scope.currentIndex = scope.images.length -1;
         scope.cleanIndex = scope.currentIndex + 1;
         $('.number-display').text(scope.cleanIndex);
+        //same as function in scope.next
+        if(window.location.href.indexOf("activity4") > -1) {
+          $('#caption').text(caption[scope.currentIndex + 7])
+        } else {
+          $('#caption').text(caption[scope.currentIndex]);
+        }
       };
       scope.$watch('currentIndex', function(){
+        //don't show images not of the current index
         scope.images.forEach(function(image){
           image.visible = false;
         });
+        //only show images on current index
         scope.images[scope.currentIndex].visible = true;
       });
     },
